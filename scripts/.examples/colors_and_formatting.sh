30828363d98f62af2d28235e5972a0f37753224a
TacB0sS <adam.vdk@nu-art-software.com>
2019-04-10 01:11:15 +0300

    amazing scripts from https://misc.flogisoft.com/bash/tip_colors_and_formatting

---
+++ scripts/.examples/colors_and_formatting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Background
+for clbg in {40..47} {100..107} 49 ; do
+	#Foreground
+	for clfg in {30..37} {90..97} 39 ; do
+		#Formatting
+		for attr in 0 1 2 4 5 7 ; do
+			#Print the result
+			echo -en "\e[${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m \e[0m"
+		done
+		echo #Newline
+	done
+done
+
+exit 0

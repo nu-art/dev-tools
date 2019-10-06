4b948ccefd4dfbf1550b161e8600e08d664ed7da
TacB0sS <adam.vdk@nu-art-software.com>
2019-10-06 14:58:04 +0300

    more animation

--- scripts/oos/tests/_test_Animation.sh
+++ scripts/oos/tests/_test_Animation.sh
@@ -11,20 +11,31 @@ function _cloud2() {
 \`- __.'"
 }
 
+function _cloud1() {
+  echo -e "   _
+ (\`  ).
+(      ).
+(     (   '\`.
+ (   )     .  )
+  (..__.:'--''"
+
+}
+
+
 function run() {
     new Canvas2D canvas
     new Drawable2D cloud1
     new Drawable2D cloud2
 
     canvas.width = 100
-    canvas.height = 8
+    canvas.height = 10
     canvas.prepare
     #canvas.clean
     #
     cloud1.animObj = "`_cloud2`"
     cloud1.init
 
-    cloud2.animObj = "`_cloud2`"
+    cloud2.animObj = "`_cloud1`"
     cloud2.init
 
     fromX=10
@@ -34,6 +45,7 @@ function run() {
     for (( x=${fromX}; x != ${toX}; x+=${dx} )); do
         canvas.prepare
         canvas.paint cloud1 ${x} 1
+        canvas.paint cloud1 $((${x} + 5)) 1
         canvas.paint cloud2 $(( 90 - ${x})) 3
         canvas.draw
         canvas.clean

07c3e6a431d44b14a88f96fd85a18c02b4a705d4
TacB0sS <adam.vdk@nu-art-software.com>
2020-01-24 22:50:57 +0200

    default modules config for ts

--- scripts/dev/typescript/modules.sh
+++ scripts/dev/typescript/modules.sh
@@ -4,11 +4,11 @@ boilerplateRepo="git@github.com:nu-art-js/thunderstorm-boilerplate.git"
 
 frontendModule=app-frontend
 backendModule=app-backend
+
 allowedBranchesForPromotion=(master staging dev)
 
 projectModules=(app-backend app-frontend)
+nuArtModules=(ts-common testelot thunder storm live-docs user-login)
+linkedSourceModules=(app-shared)
 
-otherModules=(app-shared)
-
-nuArtModules=(ts-common testelot thunder storm)
-modules=()
+dependencyModules=()

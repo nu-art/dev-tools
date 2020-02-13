7946eef2ee8527dd9520c0a9e9304ec81eb76ba0
TacB0sS <adam.vdk@nu-art-software.com>
2020-02-13 22:31:09 +0200

    import migration script

---
+++ scripts/dev/typescript/thunderstorm-migration-0.16.0.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ../../_core-tools/_source.sh
+
+replaceStringInFiles . "@nu-art/storm/server" "@nu-art/thunderstorm/backend" node_modules dist dist-test .idea .stuff .trash .fork .firebase .config dev-tools ts-common testelot thunderstorm firebase .git
+replaceStringInFiles . "@nu-art/storm/firebase" "@nu-art/firebase/backend" node_modules dist dist-test .idea .stuff .trash .fork .firebase .config dev-tools ts-common testelot thunderstorm firebase .git
+replaceStringInFiles . "\"@nu-art/thunder\":" "\"@nu-art/thunderstorm\":" node_modules dist dist-test .idea .stuff .trash .fork .firebase .config dev-tools ts-common testelot thunderstorm firebase .git
+replaceStringInFiles . "from \"@nu-art/thunder\"" "from \"@nu-art/thunderstorm/frontend\"" node_modules dist dist-test .idea .stuff .trash .fork .firebase .config dev-tools ts-common testelot thunderstorm firebase .git
+replaceStringInFiles . "\"@nu-art/firebase-functions\"" "\"@nu-art/firebase/functions\"" node_modules dist dist-test .idea .stuff .trash .fork .firebase .config dev-tools ts-common testelot thunderstorm firebase .git

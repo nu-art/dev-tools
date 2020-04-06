d53c974d288f5d8740e086ecc47b6675e5b47aea
TacB0sS <adam.vdk@nu-art-software.com>
2020-04-07 00:03:48 +0300

    signature

--- scripts/dev/typescript/fork-thunderstorm.sh
+++ scripts/dev/typescript/fork-thunderstorm.sh
@@ -21,7 +21,7 @@ firebaseProjectLocation=us-central1
 
 function signatureThunderstorm() {
   clear
-  logVerbose "${Gray}             _____ _                     _                    _                                      ${NoColor}"
+  logVerbose "${Gray} -------     _____ _                     _                    _                              ------- ${NoColor}"
   logVerbose "${Gray} -------    |_   _| |__  _   _ _ __   __| | ___ _ __      ___| |_ ___  _ __ _ __ ___    ${Gray}   ------- ${NoColor}"
   logVerbose "${Gray} -------      | | | '_ \| | | | '_ \ / _\` |/ _ \ '__|____/ __| __/ _ \| '__| '_ \` _ \   ${Gray}   ------- ${NoColor}"
   logVerbose "${Gray} -------      | | | | | | |_| | | | | (_| |  __/ | |_____\__ \ || (_) | |  | | | | | |  ${Gray}   ------- ${NoColor}"

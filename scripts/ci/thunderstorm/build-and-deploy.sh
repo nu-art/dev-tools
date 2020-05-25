c663f2253f687860c59d5280e6f2908394c5fee6
TacB0sS <adam.vdk@nu-art-software.com>
2020-05-25 09:12:57 +0300

    bad script

--- scripts/ci/thunderstorm/build-and-deploy.sh
+++ scripts/ci/thunderstorm/build-and-deploy.sh
@@ -20,13 +20,14 @@ bash ./dev-tools/scripts/git/git-checkout.sh --branch=${branch} --all
 bash ./dev-tools/scripts/git/git-pull.sh -a -f
 
 [[ "${publish}" ]] && publish="--publish=${promoteVersion}"
-[[ "${deploy}" ]] && echo "${deploy}" && deploy="-d"
-bash build-and-install.sh --debug -i -c "-se=${environment}" --test=/etc/test-account.json --log=verbose "${publish}" ${deploy}
+[[ "${deploy}" ]] && version="--set-version=$(getVersionName version-app.json).${BUILD_NUMBER}"
+[[ "${deploy}" ]] && deploy="-d"
+bash build-and-install.sh --debug -i -c "-se=${environment}" --test=/etc/test-account.json --log=verbose "${publish}" ${deploy} ${version}
 throwError "Error while publishing artifacts to NPM and deploying project"
 
 newVersionName=$(getVersionName version-thunderstorm.json)
 echo newVersionName="${newVersionName}" > ../build.properties
 
 errorMessage=$(cat ./error_message.txt)
-echo errorMessage=${errorMessage} >> ../build.properties
+echo errorMessage="${errorMessage}" >> ../build.properties
 _cd..

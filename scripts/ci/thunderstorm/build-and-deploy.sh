8880fdff49347f3be487c0dd6b8fe0d54783ccb3
TacB0sS <adam.vdk@nu-art-software.com>
2020-07-30 00:11:09 +0300

    can test specific packages that were changed

--- scripts/ci/thunderstorm/build-and-deploy.sh
+++ scripts/ci/thunderstorm/build-and-deploy.sh
@@ -31,3 +31,29 @@ echo newVersionName="${newVersionName}" > ../build.properties
 errorMessage=$(cat ./error_message.txt)
 echo errorMessage="${errorMessage}" >> ../build.properties
 _cd..
+
+# FOR FUTURE REF... CAN TEST ONLY THINGS THAT HAVE CHANGED...
+# BUT SHOULD IT TEST ALSO THE PACKAGES THAT ARE DEPENDENT AS WELL.. ??
+#
+#bash build-and-install.sh --debug --log=verbose -i -c "-se=${environment}"
+#
+#folders=()
+#usePackages=""
+#counter=0
+#lastTag="$(getVersionName version-app.json).$((BUILD_NUMBER - counter))}"
+#while gitAssertTagExists "${lastTag}"; do
+#  counter=$((counter + 1))
+#  lastTag="$(getVersionName version-app.json).$((BUILD_NUMBER - counter))}"
+#done
+#
+#if [[ "${lastTag}" ]]; then
+#  logInfo "found last tag: ${lastTag}"
+#  changes="$(git diff --dirstat=files,0 "${lastTag}" | sed -E 's/^[ 0-9\.]*% ([a-zA-Z0-9_-]*).*/\1/g')"
+#  while IFS= read -r folder; do
+#    [[ "$(array_contains "${folder}" "${folders[@]}")" ]] && continue
+#    folders+=("${folder}")
+#    usePackages="${usePackages} -up=${folder}"
+#  done <<< "$changes"
+#fi
+#bash build-and-install.sh --debug --log=verbose --account=/etc/test-account.json ${usePackages}
+#bash build-and-install.sh --debug --log=verbose "${publish}" ${deploy} ${version}

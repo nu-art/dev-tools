9d461a1d5b63dac510907d7610c12cd0e7dea603
TacB0sS <adam.vdk@nu-art-software.com>
2020-09-16 21:37:07 +0300

    wip

--- src/com/nu/art/pipeline/Docker.groovy
+++ src/com/nu/art/pipeline/Docker.groovy
@@ -90,7 +90,7 @@ public class Docker
     if (!command)
       throw new BadImplementationException("Trying to execute a command that is undefined")
 
-    pipeline.sh """docker exec ${id} \"cd ${envVariables[EnvVar_Workspace]} && eval \\"${command}\\"\" """
+    pipeline.sh """docker exec ${id} "cd ${envVariables[EnvVar_Workspace]} && eval \"${command}\"" """
     return this
   }
 

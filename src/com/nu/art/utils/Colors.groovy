d10848d8199554a96fe725ef4d47ffbd5ef8aaff
TacB0sS <adam.vdk@nu-art-software.com>
2021-01-11 13:52:36 +0200

    support abort slack

--- src/com/nu/art/utils/Colors.groovy
+++ src/com/nu/art/utils/Colors.groovy
@@ -2,7 +2,7 @@ package com.nu.art.utils
 
 class Colors {
   public static String Gray = "#AAAAAA"
-  public static String DarkGray = "#444444"
+  public static String DarkGray = "#666666"
   public static String Blue = "#0774E0"
   public static String Red = "#C70700"
   public static String Yellow = "#F5C242"

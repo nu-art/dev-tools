e4f84181b98a6d0b84ac4139eca63e4caa1bdece
TacB0sS <adam.vdk@nu-art-software.com>
2021-01-12 17:33:37 +0200

    want to see logs so not deleting

--- src/com/nu/art/pipeline/workflow/Workflow.groovy
+++ src/com/nu/art/pipeline/workflow/Workflow.groovy
@@ -130,11 +130,11 @@ class Workflow
 		currentBuild.getRawBuild().getExecutor().interrupt(Result.NOT_BUILT)
 		this.logWarning("Intentionally terminating this job: ${reason}")
 		sleep(10000)   // Interrupt is not blocking and does not take effect immediately.
-		try {
-			currentBuild.getRawBuild().delete()
-		} catch (e) {
-			this.logError("Failed delete build 3", e)
-		}
+//		try {
+//			currentBuild.getRawBuild().delete()
+//		} catch (e) {
+//			this.logError("Failed delete build 3", e)
+//		}
 	}
 
 	void run() {

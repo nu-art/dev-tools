12aac389e4efe25246b49d0eab4f8639ea24f58d
AlanBen <abseralvo@gmail.com>
2022-02-14 17:33:47 +0200

    .

--- scripts/dev/typescript-oo/classes/FrontendPackage.class.sh
+++ scripts/dev/typescript-oo/classes/FrontendPackage.class.sh
@@ -20,10 +20,10 @@ FrontendPackage() {
       echo "${value}"
     }
 
-    local target1="$(getJsonValueForKeyAndIndex "./firebase.json" "target" 1)"
+    local target1="$(getJsonValueForKeyAndIndex "../firebase.json" "target" 1)"
     ${CONST_Firebase} target:apply hosting "${target1}" "${target1}"
 
-    local target2="$(getJsonValueForKeyAndIndex "./firebase.json" "target" 2)"
+    local target2="$(getJsonValueForKeyAndIndex "../firebase.json" "target" 2)"
     ${CONST_Firebase} target:apply hosting "${target2}" "${target2}"
 
     ${CONST_Firebase} deploy --only hosting

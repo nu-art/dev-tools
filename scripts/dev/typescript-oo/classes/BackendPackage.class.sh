b5b7a037c12573fdb55ed7a6304aa34d2da0310e
TacB0sS <adam.vdk@nu-art-software.com>
2022-07-24 12:03:38 +0300

    dev--typed-api fixes

--- scripts/dev/typescript-oo/classes/BackendPackage.class.sh
+++ scripts/dev/typescript-oo/classes/BackendPackage.class.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+FOLDER_Config="${Path_RootRunningDir}/.firebase_config"
 
 BackendPackage() {
   extends class NodePackage
 
   _deploy() {
     [[ ! "$(array_contains "${folderName}" "${ts_deploy[@]}")" ]] && return
+
+    if [[ -e "${FOLDER_Config}/firestore.indexes.json" ]]; then
+      ${CONST_Firebase}
+    fi
 
     logInfo "Deploying: ${folderName}"
     ${CONST_Firebase} deploy --only functions
@@ -46,6 +51,23 @@ BackendPackage() {
 
   _launch() {
     [[ ! "$(array_contains "${folderName}" "${ts_launch[@]}")" ]] && return
+
+    logInfo "Prepare indexes and rules: ${folderName}"
+    if [[ ! -e "${FOLDER_Config}/database.rules.json" ]]; then
+      copyFileToFolder "${Path_RootRunningDir}/dev-tools/scripts/dev/typescript-oo/templates/firebase_config/database.rules.json" "${FOLDER_Config}"
+    fi
+
+    if [[ ! -e "${FOLDER_Config}/firestore.indexes.json" ]]; then
+      copyFileToFolder "${Path_RootRunningDir}/dev-tools/scripts/dev/typescript-oo/templates/firebase_config/firestore.indexes.json" "${FOLDER_Config}"
+    fi
+
+    if [[ ! -e "${FOLDER_Config}/firestore.rules" ]]; then
+      copyFileToFolder "${Path_RootRunningDir}/dev-tools/scripts/dev/typescript-oo/templates/firebase_config/firestore.rules" "${FOLDER_Config}"
+    fi
+
+    if [[ ! -e "${FOLDER_Config}/storage.rules" ]]; then
+      copyFileToFolder "${Path_RootRunningDir}/dev-tools/scripts/dev/typescript-oo/templates/firebase_config/storage.rules" "${FOLDER_Config}"
+    fi
 
     logInfo "Launching: ${folderName}"
     npm run launch

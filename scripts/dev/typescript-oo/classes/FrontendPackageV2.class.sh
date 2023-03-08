cb68117dbad94f5d865a0a286a6c301f84bad0d7
Cipher <itayleybovich@gmail.com>
2023-03-08 16:22:13 +0200

    env passed to build command

--- scripts/dev/typescript-oo/classes/FrontendPackageV2.class.sh
+++ scripts/dev/typescript-oo/classes/FrontendPackageV2.class.sh
@@ -25,7 +25,7 @@ FrontendPackageV2() {
 
     [[ -e "${Path_RootRunningDir}/version-app.json" ]] && copyFileToFolder "${Path_RootRunningDir}/version-app.json" "./src/main"
 
-    npm run build
+    ENV=${envType} npm run build
     throwWarning "Error compiling: ${folderName}"
   }
 

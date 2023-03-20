8f68e2226580621c4bcebf9802d1d0bca95180ab
TacB0sS <adam.vdk@nu-art-software.com>
2023-03-20 14:49:40 +0200

    pnpm 7.30.0

--- scripts/node/pnpm.sh
+++ scripts/node/pnpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pnpm.install() {
-  local version="${1:-"7.29.1"}"
+  local version="${1:-"7.30.0"}"
   if pnpm.isInstalled; then
     [[ "${version}" == "$(pnpm.version)" ]] && return 0
 

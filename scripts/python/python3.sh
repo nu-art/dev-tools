670bcaf7d28e68a5c225eb6a4117b2abb8feaa3b
TacB0sS <adam.vdk@nu-art-software.com>
2023-08-01 14:21:45 +0300

    with python infra script

---
+++ scripts/python/python3.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+python3.install() {
+	local version="${1:-"3.11"}"
+
+	if [[ $(python3.isInstalled) -eq 0 ]]; then
+		[[ "$(python3.version)" =~ ${version} ]] && return 0
+	fi
+
+	brew update
+	bannerInfo "Python3 - Installing v${version}"
+	brew install python@${version}
+	python3 -m ensurepip --upgrade
+	logInfo "Python3 - Installed ${version}"
+}
+
+python3.isInstalled() {
+	[[ -x "$(command -v python3)" ]] && return 0
+}
+
+python3.version() {
+	if [[ -x "$(command -v python3)" ]]; then
+		python3 --version
+	fi
+}
+
+python3.setVenv() {
+	local envName=${1}
+	logInfo "Setting up VENV - ${envName}"
+	python3 -m venv "${envName}"
+	source "${envName}/bin/activate"
+
+}
+
+python3.installRequirements() {
+	local folderName=${1:-.}
+	logInfo "PIP3 - Installing Requirements"
+	pip3 install -r "${folderName}/requirements.txt"
+}
+
+python3.uninstallRequirements() {
+	local folderName=${1:-.}
+	logInfo "PIP3 - Uninstalling Requirements"
+	pip3 uninstall -r "${folderName}/requirements.txt"
+}
+
+python3.installPackages() {
+	logInfo "PIP3 - Installing packages ${*}"
+	pip3 install "${@}"
+}
+
+python3.uninstallPackages() {
+	logInfo "PIP3 - Uninstalling packages ${*}"
+	python3 uninstall "${@}"
+}
+
+python3.run() {
+	local pathToMainFile=${1}
+	export PORT=4444
+	export PYTHONPATH=.
+	python3 "${pathToMainFile}" ${@:2}
+}
+ß

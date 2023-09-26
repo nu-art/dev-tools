181119f46d743c69dadb1864a6fbaa9493e8738b
Yuval Talby <yuval@quai.md>
2023-09-26 10:21:00 +0300

    reverted python install

--- scripts/python/python3.sh
+++ scripts/python/python3.sh
@@ -7,26 +7,11 @@ python3.install() {
 		[[ "$(python3.version)" =~ ${version} ]] && return 0
 	fi
 
-  if [[ $(isMacOS) ]]; then
-    brew update
-    bannerInfo "Python3 - Installing v${version}"
-    brew install python@${version}
-    python3 -m ensurepip --upgrade
-    logInfo "Python3 - Installed ${version}"
-  else
-    cd /tmp/
-    wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz
-    tar -xzvf Python-3.11.4.tgz
-    cd Python-3.11.4/
-    apt update
-    apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
-    ./configure --enable-optimizations
-    make -j `nproc`
-    make altinstall
-    python3.11 -V
-    sudo ln -s /usr/local/bin/python3.11 /usr/local/bin/python
-    python -VV
-  fi
+	brew update
+	bannerInfo "Python3 - Installing v${version}"
+	brew install python@${version}
+	python3 -m ensurepip --upgrade
+	logInfo "Python3 - Installed ${version}"
 }
 
 python3.isInstalled() {

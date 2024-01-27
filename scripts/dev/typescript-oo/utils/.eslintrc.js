45ab8c13d8e198a92f16210accf5d179bde63629
TacB0sS <adam.vdk@nu-art-software.com>
2024-01-27 23:25:06 +0200

    eslint

--- scripts/dev/typescript-oo/utils/.eslintrc.js
+++ scripts/dev/typescript-oo/utils/.eslintrc.js
@@ -1,30 +1,31 @@
-// FROM DEV-TOOLS
-
 module.exports = {
-	root: true,
-	"env": {
-		"es6": true,
-		"node": true
+	parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+	parserOptions: {
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+		sourceType: "module", // Allows for the use of imports
+		ecmaFeatures: {
+			jsx: true // Allows for the parsing of JSX
+		}
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended"
+	settings: {
+		react: {
+			version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+		}
+	},
+	extends: [
+		"plugin:@typescript-eslint/recommended" // Uses the recommended rules from @typescript-eslint/eslint-plugin
 	],
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
+	rules: {
+		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/ban-types": "off",
+		"@typescript-eslint/camelcase": "off",
+		"@typescript-eslint/ban-ts-comment": "off",
+		"@typescript-eslint/ban-ts-ignore": "off",
+		"@typescript-eslint/no-use-before-define": "off",
+		"@typescript-eslint/no-empty-function": "off",
+		"@typescript-eslint/class-name-casing": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/explicit-function-return-type": "off"
 	},
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint"
-	],
-	"rules": {
-		"no-mixed-spaces-and-tabs": "off",
-		"no-unused-vars": "off",
-		"no-useless-escape": "off",
-	}
 };

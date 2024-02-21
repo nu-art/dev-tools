593e40e06fe4b888b5be9d49053a4126910f51c5
Yuval Talby <yuval@quai.md>
2024-02-21 16:24:34 +0200

    added python.installvenv cmd

--- scripts/python/python3.sh
+++ scripts/python/python3.sh
@@ -13,16 +13,15 @@ python3.install() {
             else
                 # Assuming apt for non-macOS systems; adjust as necessary
                 bannerInfo "Python3 - Installing v${version}"
-                sudo apt update
-                sudo apt install -y python3 python3-pip
-				python3 --version
-				pip3 --version
+                sudo apt-get update
+                sudo apt-get install -y python3 python3-pip
+                python3 --version
+                pip3 --version
                 # Use update-alternatives to set python3 as the default python version if needed
             fi
         fi
     fi
 
-    
     logInfo "Python3 - Installed ${version}"
 }
 
@@ -33,6 +32,15 @@ python3.isInstalled() {
 python3.version() {
 	if [[ -x "$(command -v python3)" ]]; then
 		python3 --version
+	fi
+}
+
+python3.installvenv() {
+	logInfo "PIP3 - Installing packages ${*}"
+	if [[ $(isMacOS) == "true" ]]; then
+	  	pip3 install venv
+	else
+	  sudo pt-get install python3-venv
 	fi
 }
 
